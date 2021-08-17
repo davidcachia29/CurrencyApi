87b4b9dd5b787afae8a9e9a6e95fabc32edefe3c
davidcachia29 <davidcachia29@gmail.com>
2021-08-17 17:11:44 +0200

    Project Creation

---
+++ currencyConverterAPI/Models/Information.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace currencyConverterAPI.Models
+{
+    public class Information
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+    }
+}
